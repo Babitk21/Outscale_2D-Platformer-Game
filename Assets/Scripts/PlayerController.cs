d74997188a9721cfd7dc8fccecff6b8c14a9f559
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2023-09-01 01:48:23 +0530

    Done with crouch, jump mechanism.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     Animator animator;
     const string SPEED = "Speed";
+    const string CROUCH = "Crouch";
+    const string JUMP = "Jump";
     private void Start() {
         animator = GetComponent<Animator>();
     }
     private void Update() {
         float speed = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         animator.SetFloat(SPEED, Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if(speed < 0) {
@@ -20,5 +23,24 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(speed);
         }
         transform.localScale = scale;
+        Crouch();
+        Jump(vertical);
+    }
+    private void Crouch() {
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            animator.SetBool(CROUCH, true);
+        }
+        else {
+            animator.SetBool(CROUCH, false);
+        }
+    }
+    private void Jump(float vertical) {
+        if (vertical > 0) {
+            Debug.Log("Here");
+            animator.SetBool(JUMP, true);
+        }
+        else {
+            animator.SetBool(JUMP, false);
+        }
     }
 }
