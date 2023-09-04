471d098836a3dc9339876246aa09c0c5fbbae030
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2023-09-04 18:39:07 +0530

    Added enemy and movement to it

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.GetComponent<PlayerController>() != null) {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.GetComponent<PlayerController>() != null) {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             playerController.PickUpKey();
             Destroy(gameObject);
